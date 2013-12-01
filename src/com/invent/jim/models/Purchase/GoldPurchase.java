682986491c9152d82327c110fce51f3613d7d87c
anshul <dep168095@gmail.com>
2013-12-02 01:53:27 +0530

    Added methods in purchase model

--- src/com/invent/jim/models/Purchase/GoldPurchase.java
+++ src/com/invent/jim/models/Purchase/GoldPurchase.java
@@ -1,5 +1,7 @@
 package com.invent.jim.models.Purchase;
 
+import java.util.Date;
+
 import android.content.Context;
 
 import com.invent.jim.OrnamentType;
@@ -10,22 +12,33 @@ public class GoldPurchase extends SugarRecord<GoldPurchase> {
 	long date;
 	public int weight;
 	public int finalPrice;
-	public enum GoldPurchaseType{
+
+	public enum GoldPurchaseType {
 		ORNAMENT, ROUGH, FINE
 	}
+
 	public int purchaseType;
-	public OrnamentType type;
+	public int ornamentType;
 	public int tunch;
 	public int goldRate;
-	public int quantity;	
-	
+	public int quantity;
+
 	public GoldPurchase(Context arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
 	}
 
-	public GoldPurchase(Context arg0, GoldPurchaseType purchaseType, int weight, int tunch, int goldRate, OrnamentType ornamentType, int quantity) {
+	public GoldPurchase(Context arg0, int weight, int tunch, int goldRate,
+			GoldPurchaseType purchaseType, int finalPrice,
+			OrnamentType ornamentType, int quantity) {
 		super(arg0);
+		this.date = new Date().getTime();
+		this.weight = weight;
+		this.tunch = tunch;
+		this.goldRate = goldRate;
+		this.purchaseType = purchaseType.ordinal();
+		this.ornamentType = ornamentType.ordinal();
+		this.quantity = quantity;
+		this.finalPrice = finalPrice;
 	}
-	
 }
--- src/com/invent/jim/models/Purchase/SilverPurchase.java
+++ src/com/invent/jim/models/Purchase/SilverPurchase.java
@@ -1,17 +1,38 @@
 package com.invent.jim.models.Purchase;
 
+import java.util.Date;
+
+import android.content.Context;
+
 import com.invent.jim.OrnamentType;
+import com.orm.SugarRecord;
 
-public class SilverPurchase {
+public class SilverPurchase extends SugarRecord<SilverPurchase>{
 
+	public long date;
 	public int weight;
 	public int finalPrice;
 	public enum SilverPurchaseType{
 		ORNAMENT, ROUGH, FINE
 	}
-	public SilverPurchaseType purchaseType;
-	public OrnamentType type;
+	public int purchaseType;
+	public int ornamentType;
 	public int tunch;
 	public int goldRate;
 	public int quantity;
+	
+	public SilverPurchase(Context arg0, int weight, int tunch, int goldRate,
+			SilverPurchaseType purchaseType, int finalPrice,
+			OrnamentType ornamentType, int quantity) {
+		super(arg0);
+		this.date = new Date().getTime();
+		this.weight = weight;
+		this.tunch = tunch;
+		this.goldRate = goldRate;
+		this.purchaseType = purchaseType.ordinal();
+		this.ornamentType = ornamentType.ordinal();
+		this.quantity = quantity;
+		this.finalPrice = finalPrice;
+	}
+	
 }
